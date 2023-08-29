160fa3f99486f9b0c8b40f188097fd473720ff8e
Shreyans45 <94587906+Shreyans45@users.noreply.github.com>
2023-08-29 21:20:44 +0530

    Create Basic Website Scrapper.py

---
+++ Basic Website Scrapper.py
@@ -0,0 +1,24 @@
+import requests as req
+
+
+def scrap(url):
+    i = input("DO YOU WANT TO SCRAP ......Y/N: ")
+    while i.lower() == "y":
+        re = req.get(url)
+        if re.status_code == 200:
+            text = re.content
+            fn = input("Enter file name: ")
+            try:
+                with open(fn, 'wb') as fd:
+                    fd.write(text)
+                print("File saved successfully.")
+            except Exception as e:
+                print("Exception:", str(e))
+        else:
+            print("Invalid URL or request failed.")
+
+        i = input("DO YOU WANT TO SCRAP AGAIN......Y/N: ")
+
+
+url = input("Enter URL: ")
+scrap(url)
